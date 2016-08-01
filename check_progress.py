aff250fd3588b0adf70a2031ec5ee9c78018280a
edodge <edodge11@gmail.com>
2016-08-01 19:35:57 +0530

    Debug scrape progress check

--- check_progress.py
+++ check_progress.py
@@ -9,12 +9,12 @@ def check_job_card_urls():
 	msg = 'Checking the list of job card urls...\r\n'
 
 	job_card_urls = pd.read_csv(output_dir+'/job_card_urls.csv',header=None,names=['job_card','url'])
-	gp_list = pd.read_csv(input_dir+'/gp list.csv',header=None,names=['district_name','district_code','block_name','block_code','panchayat_name','panchayat_code','treatment_status'])
+	gp_list = pd.read_csv(input_dir+'/gp list.csv',header=None,names=['district_name','district_code','block_name','block_code','panchayat_name','panchayat_code','treatment_status'],dtype={'panchayat_code':object})
 
 	job_card_urls['panchayat_code'] = job_card_urls.url.apply(lambda x: x.split('Panchayat_Code=')[1].split('&')[0])
 	job_card_urls = job_card_urls[['panchayat_code','job_card']].drop_duplicates().groupby(['panchayat_code']).count().reset_index()
 
-	job_card_counts = pd.merge(gp_list[['panchayat_code']],jobcards[['job_card_number']].drop_duplicates(),how='left',on='panchayat_code')
+	job_card_counts = pd.merge(gp_list[['panchayat_code']],job_card_urls,how='left',on='panchayat_code')
 
 	job_card_counts = job_card_counts.fillna(0)
 
@@ -32,11 +32,11 @@ def check_job_card_urls():
 
 def check_job_card_scrape():
 	msg = 'Checking the progress of the job card scrape against the list of job card urls...\r\n'
-	
+
 	jobcards = pd.read_csv(output_dir+'/jobcard.csv')
 	job_card_urls = pd.read_csv(output_dir+'/job_card_urls.csv',header=None,names=['job_card','url'])
-	jc_df = pd.merge(job_card_urls,jobcards[['job_card_number']].drop_duplicates(),how='left',on_left='job_card',on_right='job_card_number')
-	
+	jc_df = pd.merge(job_card_urls,jobcards[['job_card_number']].drop_duplicates(),how='left',left_on='job_card',right_on='job_card_number')
+
 	jc_notscraped_df = jc_df[pd.isnull(jc_df.job_card_number)][['job_card','url']]
 
 	if len(jc_notscraped_df.index)==0:
@@ -44,8 +44,8 @@ def check_job_card_scrape():
 	else:
 		jc_total = len(jc_df.index)
 		jc_scraped = jc_total - len(jc_notscraped_df.index)
-		jc_pct = "%.1f" % (float(jc_scraped)/float(jc_total))*100
-		msg += '%d of %d job cards have been scraped (%s%)\r\n' % (jc_scraped,jc_total,jc_pct)
+		jc_pct = (float(jc_scraped)/float(jc_total))*100
+		msg += '%d of %d job cards have been scraped (%s%)\r\n' % (jc_scraped,jc_total,'%.1f' % jc_pct)
 
 	msg += '\r\n'
 	return msg
