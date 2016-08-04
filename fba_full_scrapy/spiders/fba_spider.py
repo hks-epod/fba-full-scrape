1004c6aaa5cc4e59c197c67c4ff06d27623c71d0
edodge <edodge11@gmail.com>
2016-08-04 14:37:49 +0530

    Handle job card parse errors

--- fba_full_scrapy/spiders/fba_spider.py
+++ fba_full_scrapy/spiders/fba_spider.py
@@ -14,6 +14,7 @@ from scrapy import signals
 import sys
 import urlparse
 import pandas as pd
+import logging
 
 muster_roll_codes = []
 colors = {'active':['#00CC33','#D39027'],'inactive':['Red','Gray']}
@@ -188,70 +189,73 @@ class MySpider(CrawlSpider):
         url = response.url
         url = url.replace('%20',' ').strip()
         #Get top-level job card info
-        panchayat = url.split('Panchayat_Code=')[1].split('&')[0]
-        job_card = url.split('reg_no=')[1].split('&')[0]
+        try:
+            panchayat = url.split('Panchayat_Code=')[1].split('&')[0]
+            job_card = url.split('reg_no=')[1].split('&')[0]
 
-        job_card_table = soup.find_all('table')[1]
-        job_card_rows = job_card_table.find_all('tr')
-        top_data = list()
-        top_data = [panchayat,unidecode(job_card_rows[2].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[3].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[5].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(soup.find_all('td')[13].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[6].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[7].find_all('td')[1].text.encode('utf-8').decode('utf-8')),'',unidecode(job_card_rows[9].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[10].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[11].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[12].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[12].find_all('td')[3].text.encode('utf-8').decode('utf-8'))]
-        bottom_data = list()
-        person_table = job_card_table.find_all('table')[0]
-        j=-1
-        for tr in person_table:
-            j += 1
-            if j>1 and j<len(person_table)-1:
-                # Add job card level and individual level data
-                bottom_data = [unidecode(tr.find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[2].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[3].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[4].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[5].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[6].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[7].text.encode('utf-8').decode('utf-8'))]
-                item_data = top_data+bottom_data
-                item_data = [item.strip() for item in item_data]
-                item = JobcardItem()
-                item['panchayat_code'] = item_data[0]
-                item['job_card_number'] = item_data[1]
-                item['head_of_hh_name'] = item_data[2]
-                item['father_husband_name'] = item_data[3]
-                item['sc_st_category'] = item_data[4]
-                item['reg_date'] = item_data[5]
-                item['address'] = item_data[6]
-                item['village_name'] = item_data[7]
-                item['panchayat_name'] = item_data[8]
-                item['block_name'] = item_data[9]
-                item['district_name'] = item_data[10]
-                item['bpl_status'] = item_data[11]
-                item['family_id'] = item_data[12]
-                item['person_id'] = item_data[13]
-                item['applicant_name'] = item_data[14]
-                item['applicant_gender'] = item_data[15]
-                item['applicant_age'] = item_data[16]
-                item['account_no'] = item_data[17]
-                item['bank_po_name'] = item_data[18]
-                item['aadhar_no'] = item_data[19]
-                yield item
+            job_card_table = soup.find_all('table')[1]
+            job_card_rows = job_card_table.find_all('tr')
+            top_data = list()
+            top_data = [panchayat,unidecode(job_card_rows[2].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[3].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[5].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(soup.find_all('td')[13].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[6].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[7].find_all('td')[1].text.encode('utf-8').decode('utf-8')),'',unidecode(job_card_rows[9].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[10].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[11].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[12].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[12].find_all('td')[3].text.encode('utf-8').decode('utf-8'))]
+            bottom_data = list()
+            person_table = job_card_table.find_all('table')[0]
+            j=-1
+            for tr in person_table:
+                j += 1
+                if j>1 and j<len(person_table)-1:
+                    # Add job card level and individual level data
+                    bottom_data = [unidecode(tr.find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[2].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[3].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[4].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[5].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[6].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[7].text.encode('utf-8').decode('utf-8'))]
+                    item_data = top_data+bottom_data
+                    item_data = [item.strip() for item in item_data]
+                    item = JobcardItem()
+                    item['panchayat_code'] = item_data[0]
+                    item['job_card_number'] = item_data[1]
+                    item['head_of_hh_name'] = item_data[2]
+                    item['father_husband_name'] = item_data[3]
+                    item['sc_st_category'] = item_data[4]
+                    item['reg_date'] = item_data[5]
+                    item['address'] = item_data[6]
+                    item['village_name'] = item_data[7]
+                    item['panchayat_name'] = item_data[8]
+                    item['block_name'] = item_data[9]
+                    item['district_name'] = item_data[10]
+                    item['bpl_status'] = item_data[11]
+                    item['family_id'] = item_data[12]
+                    item['person_id'] = item_data[13]
+                    item['applicant_name'] = item_data[14]
+                    item['applicant_gender'] = item_data[15]
+                    item['applicant_age'] = item_data[16]
+                    item['account_no'] = item_data[17]
+                    item['bank_po_name'] = item_data[18]
+                    item['aadhar_no'] = item_data[19]
+                    yield item
 
-        muster_links = [link for link in response.xpath("//@href").extract() if 'musternew.aspx' in link]
-        # Get links to all muster rolls that individual has been listed on.
-        for link in muster_links:
-            # work_code = link.split('workcode=')[1].split('&panchayat_code')[0]
-            # msr_no = link.split('msrno=')[1].split('&finyear')[0]
-            par = urlparse.parse_qs(urlparse.urlparse(link).query)
-            work_code = par['workcode'][0]
-            msr_no = par['msrno'][0]
-            dt_from = par['dtfrm'][0]
-            day = int(dt_from[0:2])
-            month = int(dt_from[3:5])
-            year = int(dt_from[6:])
-            dt = datetime.datetime(year,month,day)
-            # with open(output_dir+'/dates.csv', 'a') as f:
-            #     writer = csv.writer(f)
-            #     writer.writerow([dt_from,day,month,year,dt])
+            muster_links = [link for link in response.xpath("//@href").extract() if 'musternew.aspx' in link]
+            # Get links to all muster rolls that individual has been listed on.
+            for link in muster_links:
+                # work_code = link.split('workcode=')[1].split('&panchayat_code')[0]
+                # msr_no = link.split('msrno=')[1].split('&finyear')[0]
+                par = urlparse.parse_qs(urlparse.urlparse(link).query)
+                work_code = par['workcode'][0]
+                msr_no = par['msrno'][0]
+                dt_from = par['dtfrm'][0]
+                day = int(dt_from[0:2])
+                month = int(dt_from[3:5])
+                year = int(dt_from[6:])
+                dt = datetime.datetime(year,month,day)
+                # with open(output_dir+'/dates.csv', 'a') as f:
+                #     writer = csv.writer(f)
+                #     writer.writerow([dt_from,day,month,year,dt])
 
 
-            if [work_code,msr_no] not in muster_roll_codes and dt>=datetime.datetime(2015,9,1):
-                muster_roll_codes.append([work_code,msr_no])
-                muster_url = ('http://164.100.129.6/netnrega'+link[2:]).replace(';','').replace('%3b','').replace('-','%96').replace('%20','+').replace('!','')
-                with open(output_dir+'/encountered_muster_links.csv', 'a') as f:
-                    writer = csv.writer(f)
-                    writer.writerow([job_card.encode('utf-8'), url.encode('utf-8'), msr_no.encode('utf-8'), muster_url.encode('utf-8')])
-                
-                yield Request(muster_url, callback=self.handle_muster, priority=1)
-                
+                if [work_code,msr_no] not in muster_roll_codes and dt>=datetime.datetime(2015,9,1):
+                    muster_roll_codes.append([work_code,msr_no])
+                    muster_url = ('http://164.100.129.6/netnrega'+link[2:]).replace(';','').replace('%3b','').replace('-','%96').replace('%20','+').replace('!','')
+                    with open(output_dir+'/encountered_muster_links.csv', 'a') as f:
+                        writer = csv.writer(f)
+                        writer.writerow([job_card.encode('utf-8'), url.encode('utf-8'), msr_no.encode('utf-8'), muster_url.encode('utf-8')])
+                    
+                    yield Request(muster_url, callback=self.handle_muster, priority=1)
+        
+        except:
+            logging.info("Couldn't parse the job card response for "+response.url)    
