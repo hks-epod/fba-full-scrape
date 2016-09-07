9da67222232e9ca5437c8dbb32c8e240bd49a12d
Eric Dodge <edodge11@gmail.com>
2016-09-07 12:02:15 -0400

    Update timeout to 710 minutes

--- all_job_cards.sh
+++ all_job_cards.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /home/ec2-user/fba-full-scrape
 git checkout separated_scrape
-timeout -s SIGINT 11h /usr/local/bin/scrapy crawl all_job_cards
+timeout -s SIGINT 710m /usr/local/bin/scrapy crawl all_job_cards
