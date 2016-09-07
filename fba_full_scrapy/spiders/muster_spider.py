701fe536410259d08d3d2201773bb0f8150b26b0
Eric Dodge <edodge11@gmail.com>
2016-09-06 20:56:50 -0400

    Fix typo in muster spider

--- fba_full_scrapy/spiders/muster_spider.py
+++ fba_full_scrapy/spiders/muster_spider.py
@@ -65,7 +65,7 @@ class MySpider(CrawlSpider):
         # logging.info("Made it to the muster page for "+url) 
         if soup.find_all('table')[2].find('b').text!='The Values specified are wrong, Please enter Proper values' and soup.find("span", {"id": "ctl00_ContentPlaceHolder1_lblMsrNo2"})!=None:
             par = urlparse.parse_qs(urlparse.urlparse(url).query)
-            panchayat = par['panchayat_code='[0]
+            panchayat = par['panchayat_code'][0]
             mrTopData = [
                 unidecode(soup.find("span", {"id": "ctl00_ContentPlaceHolder1_lblMsrNo2"}).text.encode('utf-8').strip().decode('utf-8')),
                 unidecode(soup.find("span", {"id": "ctl00_ContentPlaceHolder1_lbldatefrom"}).text.encode('utf-8').strip().decode('utf-8')),
