fe77e2a8acd1b46b553bb1807d671135c3fbb992
Eric Dodge <edodge11@gmail.com>
2016-09-20 11:09:29 -0400

    check if individuals are on the job card

--- fba_full_scrapy/spiders/jobcard_spider.py
+++ fba_full_scrapy/spiders/jobcard_spider.py
@@ -134,39 +134,8 @@ class MySpider(CrawlSpider):
         top_data = list()
         top_data = [panchayat,unidecode(job_card_rows[2].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[3].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[5].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(soup.find_all('td')[13].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[6].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[7].find_all('td')[1].text.encode('utf-8').decode('utf-8')),'',unidecode(job_card_rows[9].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[10].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[11].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[12].find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(job_card_rows[12].find_all('td')[3].text.encode('utf-8').decode('utf-8'))]
         bottom_data = list()
-        person_table = job_card_table.find_all('table')[0]
-        j=-1
-        for tr in person_table:
-            j += 1
-            if j>1 and j<len(person_table)-1:
-                # Add job card level and individual level data
-                bottom_data = [unidecode(tr.find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[2].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[3].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[4].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[5].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[6].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[7].text.encode('utf-8').decode('utf-8'))]
-                item_data = top_data+bottom_data
-                item_data = [item.strip() for item in item_data]
-                item = JobcardItem()
-                item['panchayat_code'] = item_data[0]
-                item['job_card_number'] = item_data[1]
-                item['head_of_hh_name'] = item_data[2]
-                item['father_husband_name'] = item_data[3]
-                item['sc_st_category'] = item_data[4]
-                item['reg_date'] = item_data[5]
-                item['address'] = item_data[6]
-                item['village_name'] = item_data[7]
-                item['panchayat_name'] = item_data[8]
-                item['block_name'] = item_data[9]
-                item['district_name'] = item_data[10]
-                item['bpl_status'] = item_data[11]
-                item['family_id'] = item_data[12]
-                item['person_id'] = item_data[13]
-                item['applicant_name'] = item_data[14]
-                item['applicant_gender'] = item_data[15]
-                item['applicant_age'] = item_data[16]
-                item['account_no'] = item_data[17]
-                item['bank_po_name'] = item_data[18]
-                item['aadhar_no'] = item_data[19]
-                yield item
 
-        if person_table.find_all('tr')==None:
+        if len(job_card_table.find_all('table'))==1:
             
             # Add just the job card level data
             item_data = top_data
@@ -186,7 +155,39 @@ class MySpider(CrawlSpider):
             item['bpl_status'] = item_data[11]
             item['family_id'] = item_data[12]
             yield item
-
+        
+        else:
+            person_table = job_card_table.find_all('table')[0]
+            j=-1
+            for tr in person_table:
+                j += 1
+                if j>1 and j<len(person_table)-1:
+                    # Add job card level and individual level data
+                    bottom_data = [unidecode(tr.find_all('td')[1].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[2].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[3].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[4].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[5].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[6].text.encode('utf-8').decode('utf-8')),unidecode(tr.find_all('td')[7].text.encode('utf-8').decode('utf-8'))]
+                    item_data = top_data+bottom_data
+                    item_data = [item.strip() for item in item_data]
+                    item = JobcardItem()
+                    item['panchayat_code'] = item_data[0]
+                    item['job_card_number'] = item_data[1]
+                    item['head_of_hh_name'] = item_data[2]
+                    item['father_husband_name'] = item_data[3]
+                    item['sc_st_category'] = item_data[4]
+                    item['reg_date'] = item_data[5]
+                    item['address'] = item_data[6]
+                    item['village_name'] = item_data[7]
+                    item['panchayat_name'] = item_data[8]
+                    item['block_name'] = item_data[9]
+                    item['district_name'] = item_data[10]
+                    item['bpl_status'] = item_data[11]
+                    item['family_id'] = item_data[12]
+                    item['person_id'] = item_data[13]
+                    item['applicant_name'] = item_data[14]
+                    item['applicant_gender'] = item_data[15]
+                    item['applicant_age'] = item_data[16]
+                    item['account_no'] = item_data[17]
+                    item['bank_po_name'] = item_data[18]
+                    item['aadhar_no'] = item_data[19]
+                    yield item
 
 
         muster_links = [link for link in response.xpath("//@href").extract() if 'musternew.aspx' in link]
