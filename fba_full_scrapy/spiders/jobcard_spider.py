8f2eb59000f4d6f55180a6dcc35ee0b45b4f5440
Eric Dodge <edodge11@gmail.com>
2016-09-21 11:07:06 -0400

    Revert to normal job card url list

--- fba_full_scrapy/spiders/jobcard_spider.py
+++ fba_full_scrapy/spiders/jobcard_spider.py
@@ -87,7 +87,7 @@ class MySpider(CrawlSpider):
         else:
             jobcards = pd.DataFrame({'job_card_number':[]},dtype=object)
     
-        job_card_urls = pd.read_csv(output_dir+'/job_card_urls_48.csv',header=None,names=['job_card','url']) # get the master list of job card urls to scrape
+        job_card_urls = pd.read_csv(output_dir+'/job_card_urls.csv',header=None,names=['job_card','url']) # get the master list of job card urls to scrape
 
         jc_df = pd.merge(job_card_urls,jobcards.drop_duplicates(),how='left',left_on='job_card',right_on='job_card_number')
         
@@ -99,7 +99,7 @@ class MySpider(CrawlSpider):
 
     # start/restart business goes here
     
-    if not os.path.isfile(output_dir+'/job_card_urls_48.csv'): # this is the first time through the scrape, need to populate the inital list of job card urls
+    if not os.path.isfile(output_dir+'/job_card_urls.csv'): # this is the first time through the scrape, need to populate the inital list of job card urls
         start_urls = populate_job_card_urls()
         mr_tracker = pd.DataFrame({'work_code':[],'msr_no':[]},dtype=object)
 
