777acf5759be5b8f5c6e3f0e6c0f8deaa44ab20f
Eric Dodge <edodge11@gmail.com>
2017-01-05 13:36:53 -0500

    Update timeout

--- all_job_cards.sh
+++ all_job_cards.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd /home/ec2-user/fba-full-scrape
 git checkout separated_scrape
-# timeout -s SIGINT 710m /usr/local/bin/scrapy crawl all_job_cards
-timeout -s SIGINT 630m /usr/local/bin/scrapy crawl all_job_cards
+timeout -s SIGINT 710m /usr/local/bin/scrapy crawl all_job_cards
 /usr/bin/python check_progress.py
