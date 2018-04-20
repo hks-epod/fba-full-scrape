f289b357b0a541875751014c6bc8e0d0aa1183a0
Eric Dodge <edodge11@gmail.com>
2018-04-20 12:07:27 -0400

    cleanup and update shell files

--- all_job_cards.sh
+++ all_job_cards.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /home/ec2-user/fba-full-scrape
-git checkout separated_scrape
+git checkout master
 timeout -s SIGINT 710m /usr/local/bin/scrapy crawl all_job_cards
 /usr/bin/python check_progress.py
--- all_musters.sh
+++ all_musters.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /home/ec2-user/fba-full-scrape
-git checkout separated_scrape
+git checkout master
 /usr/local/bin/scrapy crawl all_musters
 /usr/bin/python check_progress.py
